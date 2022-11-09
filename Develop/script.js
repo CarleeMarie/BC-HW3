cbb3b49ff0824135a3edba4720a18243c59f1014
Carlee Miller <carlee.miller@gmail.com>
2022-11-08 20:15:41 -0600

    starting to get the code down

--- Develop/script.js
+++ Develop/script.js
@@ -16,25 +16,41 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 */
 
-/* Steps:
-Step 1: a pop up box appeara that has a button to generate a password.
-
-Step 2: the button is pressed and another pop up box appears.
-
-Step 3: the new pop up box asks how long I want the password to be - between 8 and 128
-
-Step 4: then a pop up appears with a list of criteria to choose from: lowercase, uppercase, numeric and/or special characters
-
-Step 5: after selecting criteria, the input should be validated "You chose to have a password with , x , y , z. A confirm/go button is also no this pop up. At least one type of character must be selected. If not, an error prompt will appear.
-
-Step 6: password is generated using selected criteria
-
-Step 7: Password is diplayed in an alert or written to the page.
+/*Steps
+* 1. click button
+* 2. PROMPTS -> CONFIRM- presented with series of prompts for passwoprd criteria
+*  - PROMPT password length [8-128] chars
+*  - confirm length
+* -  PROMPT character types to include in the password
+* -  confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+* -  input should be validated and at least one character type should be selected- not NULL
+* 3. WHEN all prompts are answered THEN a password is generated that matches the selected criteria
+* 4. THEN the password is either displayed in an alert or written to the page
 */
-
-
 // Assignment code here
+//need to define the generatePassword function:
+function generatePassword() {
+  var passwordLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.")
+    console.log(passwordLength);
+  
+  if (passwordLength < 8) {
+    alert("Number must be between 8 and 128.");
+    //want to go back to the beginning of the function, so we call it
+  generatePassword();     
+  }
+  else if (passwordLength > 128) {
+    alert("Number must be between 8 and 128.");
+  }
 
+  var passwordCharacters = prompt("Which character types do you want to include? You can enter: lowercase, uppercase, numbers and special characters. Separate each type of character you list with a comma.")
+  console.log(passwordCharacters);
+
+
+
+
+
+  return "spaghetti";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
