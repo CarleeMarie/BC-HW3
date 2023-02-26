8b8906aa6efc70dbefe6f761e47d1396398902e5
Carlee Miller <carlee.miller@gmail.com>
2023-02-26 00:06:31 -0600

    trying to get this to work so I can learn what I missed

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,70 +16,83 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 */
 
-/* Steps
-* 1. click button
-* 2. PROMPTS -> CONFIRM- presented with series of prompts for passwoprd criteria
-*  - PROMPT password length [8-128] chars
-*  - confirm length
-* -  PROMPT character types to include in the password
-* -  confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-* -  input should be validated and at least one character type should be selected- not NULL
-* 3. WHEN all prompts are answered THEN a password is generated that matches the selected criteria
-* 4. THEN the password is either displayed in an alert or written to the page
-*/
-// Assignment code here
-//need to define the generatePassword function:
-function generatePassword() {
-  var passwordLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.")
-    console.log(passwordLength);
-  
-  if (passwordLength < 8) {
-    alert("Number must be between 8 and 128.");
-    //an answer that doesn't meet the criteria needs to be corrected, so we want to go back to the beginning of the function, so we call it
-    generatePassword();     
-  
-  } else if (passwordLength > 128) {
-    alert("Number must be between 8 and 128.");
-    generatePassword(); 
-  }
+// Assignment code starts here
 
+// Set up variables
+var password = document.getElementById("password");
+var availLC = "abcdefghijklmnopqrstuvwxyz";
+var availUC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var availSC = "!@#$%^&*()+"; 
+var availNum = "0123456789";
+
+
+//Define the generatePassword function:
+function generatePassword() {
+  // start of password length code block
+  var passwordLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.")
+    if (passwordLength < 8) {
+      alert("Number must be between 8 and 128.");
+      //an answer that doesn't meet the criteria needs to be corrected, so we want to go back to the beginning of the function, so we call it
+      generatePassword();    
+
+    } else if (passwordLength > 128) {
+      alert("Number must be between 8 and 128.");
+      generatePassword(); 
+    
+    // } else { 
+      //TODO: what do I put here?? I want it to move to the next variable and remember the password length chosen.
+
+    // }
+      
+      
+    }
+
+    console.log(passwordLength);
   //end of password length code block
+
   //start of character type code block
-/*  
+  var lowerCase = confirm("Please select character types you want to include in your password: \n Include lowercase letters? Yes or No?");
+    if (lowerCase) {
+      selected = lowerCase;
+      } else (!selected) {
+        // ????
+        // what do I use instead of console.log?
+        
+      }
+    
+  var upperCase = confirm("Do you want to include uppercase letters?");
+    if (selected) {
+    selected = upperCase; 
+    } else {
+
+    }
+     
+  var specialChar = confirm('Do you want to include special characters (ex: $, %, &, etc.)?');
+  if (selected) {
+    selected = specialChar; 
+  } else {
+
+  }
+  
+  var number = confirm("Do you want to include numbers?");
+  if (selected) {
+    selected = number;
+  } else {  
+   
+  }
+ 
+  
   var lowerCase = false, upperCase = false, number = false, special = false;
-  while (lowerCase === false && upperCase === false && number === false && special === false) {
+  while (lowerCase === false && upperCase === false && number === false && specialChar === false) {
     // keeping track of whether something has been selected
     var selected = false;
-    lowerCase = confirm("Do you want to include lowercase letters in your password? Yes or No?");
-  if (lowerCase) {
-    selected = lowerCase;  
-  }
   
-  upperCase = confirm("Do you want to include uppercase letters in your password? Yes or No?");
-  if (!selected) {
-    selected = upperCase;  
-  }
-
+  var concatValues = [lowerCase, upperCase] 
 
   }
+ 
+  return password;
 
-
-//if nothing selected - give a prompt that they have to select one
-//using selected length, create an empty array - use a method to figure out how to pick the values
-//if use string use a split method to randomly select value
-//check confirms , pick uppwerCase and push it in to the password
-//random.math
-//once you have all that are required, you need to run it as many times as needede to get the length. you can either get the required characters and then fill the rest with random of one character K#n7IODOSFJ 
-
-//store the selected characters in an array (see methods, maybe .push like we did with planets and then use the .join method to combine all the elements into single array/string that will then be shown to the user)
-    
- */   
-
-
-
-
-  return "spaghetti";
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -88,10 +101,36 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// call the function to start the process
+generatePassword();
+
+// This is code from exercise 3.1.10
+// Change the values and operators below to test your algorithm meets all conditions
+var x = 60;
+var expression1 = (x > 25);
+var expression2 = (x > 50);
+
+// Write Your JavaScript Code Here
+if (expression1 && expression2) {
+    console.log("True ✅ True ✅");
+  //add the bang to expression2 to make it false  
+} else if (expression1 || !expression2) {
+    console.log("True ✅ False ❌");
+  //put the bang at the front of expression1 to make it false rather than expression2  
+} else if (!expression1 || expression2) {
+    console.log("False ❌ True ✅");
+  //add the bang in front of both expressions so that they both are false  
+} else if (!expression1 && !expression2) {
+    console.log("False ❌ False ❌");
+} else {
+    console.log("One of your numbers may be undefined.")
+    //this will let you know if you have a mistake in your conditionals or code. Can delete this conditional when your code works.
+}
+
+/*NOTE: To make the code skip to the next else if statement, you need to change *the variable definitions - so change the var x equals statement or other var *value to make each conditional true. */
